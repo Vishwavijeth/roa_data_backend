9d238250fc2c0756054bf316bb0a26ab2f063335
vichu <vichucoding@gmail.com>
2026-06-18 15:31:38 +0530

    updated cors origin

--- main.py
+++ main.py
@@ -37,11 +37,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://roa-data-ui.vercel.app",
-        "http://127.0.0.1:5173",
-        "http://localhost:5173/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
