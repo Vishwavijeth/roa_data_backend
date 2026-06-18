f80f8876c2934b93a75b2015812a92bf073ec184
vichu <vichucoding@gmail.com>
2026-06-18 15:30:23 +0530

    updated cors origin.

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ app.add_middleware(
     allow_origins=[
         "https://roa-data-ui.vercel.app",
         "http://127.0.0.1:5173",
+        "http://localhost:5173/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
